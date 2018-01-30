17626a7f75b1b9ea9c7fab3182f7e09d71752f40
Ignacio Ureta <ignacio.ureta@live.com>
2018-01-29 23:06:54 -0300

    Added all types of Products to after30Days to show all of them

--- src/after-30-days.js
+++ src/after-30-days.js
@@ -1,9 +1,12 @@
 import CarInsurance from './carInsurance'
-import { Product } from './products'
+import { Product, FullCoverage, SpecialFullCoverage, MegaCoverage, SuperSale } from './products'
 
 var carInsurance = new CarInsurance([
   new Product('Low Coverage', 10, 20),
-  new Product('Low Coverage', 20, 30)
+  new FullCoverage(20, 30),
+  new SpecialFullCoverage(10, 20),
+  new MegaCoverage(0, 200),
+  new SuperSale(20, 200),
 ])
 
 carInsurance.after30Days()
--- src/products/index.js
+++ src/products/index.js
@@ -1,6 +1,13 @@
 import Product from './product'
+import FullCoverage from './fullCoverage'
+import SpecialFullCoverage from './specialFullCoverage'
+import MegaCoverage from './megaCoverage'
+import SuperSale from './superSale'
 
 module.exports = {
   Product,
-  
+  FullCoverage,
+  SpecialFullCoverage,
+  MegaCoverage,
+  SuperSale
 }
