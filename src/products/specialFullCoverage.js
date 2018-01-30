65012cf0ebb5271e2f14638890084652ebee3611
Ignacio Ureta <ignacio.ureta@live.com>
2018-01-29 22:04:15 -0300

    fixed bug with specialFullCoverage

--- src/products/specialFullCoverage.js
+++ src/products/specialFullCoverage.js
@@ -5,9 +5,9 @@ class SpecialFullCoverage extends FullCoverage {
     super(sellIn, price, "Special ")
   }
   updatePrice(){
-    super.updatePrice()
     if(this.sellIn < 11 && this.price < 50) this.price += 1
     if(this.sellIn < 6 && this.price < 50) this.price += 1
+    super.updatePrice()
 
     return this
   }
